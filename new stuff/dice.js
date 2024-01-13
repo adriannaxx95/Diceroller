6d7f50d59cf5288ded16284b7263ec8aac6bbe42
Adriannaxx95 <adriannaxx95@gmail.com>
2024-01-13 12:37:30 -0500

    add new stuff directory

---
+++ new stuff/dice.js
@@ -0,0 +1,24 @@
+function diceRoll(num, num2, Name1) {
+    console.log("this is how many sides:", num, num2)
+    document.getElementById(Name1).innerHTML = numberMinMax(num,num2);
+}
+
+function d6() {
+    console.log("button clicked")
+    console.log("this is the d6 dice")
+    numberMinMax(1, 6); // calls numberMinMax function defined above
+
+    }
+
+    function d8() {
+        console.log("button clicked")
+        console.log("this is the d8 dice")
+        numberMinMax(1, 8); // calls numberMinMax function defined above
+
+    }
+
+    function numberMinMax(min, max) {
+const diceRoll = Math.floor(Math.random() * (max - min + 1)) + min
+console.log(diceRoll) 
+return diceRoll;
+    }
